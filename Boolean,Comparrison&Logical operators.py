ab1e704c4346e3405f0068e10fef143d5eb11919
Subham2901 <subham.reek@gmail.com>
2020-05-10 04:23:34 +0530

    Create Boolean,Comparrison&Logical operators.py

---
+++ Boolean,Comparrison&Logical operators.py
@@ -0,0 +1,21 @@
+"""Boolean values are two constants objects which are true and false
+Comparison Operator= These are the  operators which returns boolean values (True/False)
+ they are "==" ,"!=",">=","<=",">","<"
+ They are used to compare between two values or variables"""
+x=90
+y=95
+print(y>x) # Returns true as 95>90
+print(x>y)# returns false as x is not greater than y.
+"""-----------------------------------------------------------------------------------------------------------------------"""
+"""-----------------------------------------------------------------------------------------------------------------------"""
+"""LOGICAL OPERATORS = These are the operators used to perform logical operations between two or more variables.
+The operators are "and","or","not
+and Returns True when both the values are true
+or returns True when either of the values are true
+not if the condition is True then the logical not operator makes it false" """
+x=10;
+z=5
+y=2
+print(x>y and y>z ) # returns False as y is not greater than z.
+print(x>y or y>z ) # returns True as one of the argument is True.
+print(not z>x)# returns True as the actual outcome is  False.
